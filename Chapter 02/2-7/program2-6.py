5a02b1b040db4c9521f5a873660f8546c1941ccb
seong in Hong <75526895+espada105@users.noreply.github.com>
2024-04-04 16:10:17 +0900

    0404
    class

---
+++ Chapter 02/2-7/program2-6.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import sys
+
+img = cv.imread('우사기.webp')
+
+if img is None:
+    sys.exit('파일을 찾을 수 없습니다')
+
+cv.rectangle(img, (130, 30), (1000, 200), (0, 0, 255), 2)
+cv.putText(img, 'laugh', (830, 24), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+cv.imshow('Draw', img)
+cv.waitKey(0)  # 사용자가 키 입력을 대기합니다. 0은 무한 대기를 의미합니다.
+cv.destroyAllWindows()
