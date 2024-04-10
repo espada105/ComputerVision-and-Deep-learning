8519a5fec4260b96824da75b547f61e310fd1a0c
desktop seongin hong <tjddls207@naver.com>
2024-04-10 13:37:21 +0900

    up

---
+++ Chapter 02/program2-1.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.array([4,5,1,23,3,3,1,2,3,4])
+print(a)
+print(type(a))
+print(np.shape(a))
+a.sort()
+print(a)
+
+b = np.array([-4.3,-2.3,1,2,2,3,14])
+b.sort()
+print(b)
+
+c = np.array(['one','two','three','four','five','six','seven'])
+c.sort()
+print(c)
